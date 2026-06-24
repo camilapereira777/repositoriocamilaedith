f62a84e0034d37dd5329ee63293c80fe348c33bd
camila pereira <>
2026-06-23 21:37:06 -0300

    esta es mi primer version

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+namespace milibreria
+{
+    class avion
+    {
+        public string color;
+        public string marca;
+        public int matricula;
+        public int capacidad;
+
+
+        
+        public void Volar()
+        {
+            Console.WriteLine("volando");
+        }
+        public void aterizar()
+        {
+            Console.WriteLine("aterizando");
+        }
+        public void despegar()
+        {
+            Console.WriteLine("despegando");
+        }
+    }
+}
---
+++ ejercicio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using milibreria;
+
+class program 
+
+{
+    static void Main()
+    {
+        avion boing = new avion();
+
+        boing.aterizar();
+        boing.despegar();
+        boing.aterizar();
+        
+    }
+}
